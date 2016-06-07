784f255e82b7a89b1c9b6f0df4083884d0a47e12
sbuberl <sbuberl@gmail.com>
2016-06-06 21:02:26 -0400

    Refactored fsql site to use JS instead of PHP
    Can now host on GitHub Pages
    Adds jQuery and Bootstrap

--- sidebar.php
+++
@@ -1,7 +0,0 @@
-    <p class="title"><a href="/index.php">Home</a></p>
-    <ul>
-      <li class="highlight"><a href="/index.php">Overview</a>
-        <span class="hidden">(this page)</span></li>
-      <li><a href="http://sourceforge.net/projects/fsql" rel="external">Project Page</a></li>
-      <li><a href="https://sourceforge.net/apps/trac/fsql" rel="external">Trac Page</a></li>
-    </ul>
